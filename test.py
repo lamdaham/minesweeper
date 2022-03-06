93b13e103a84e8d87ad8961f4ed078b392dd32fb
AAWorks <axalonso12@gmail.com>
2022-03-06 12:01:57 -0500

    ignore

--- test.py
+++ test.py
@@ -1,16 +1,41 @@
 import string
 
-n = int(input("People: "))
-
-options_dict = {ascii_lowercsae[x]: 0 for x in range(n)}
-
-for i in range(n):
-    print("a) Daddys Home\nb) Zootopia\nc) A New Hope\nd) Fantastic Beasts")
-    options = input("Movie Ranking (i.e. a,b,c,d): ").strip().split(",")
+class Poll:
+    def __init__(self, people, selections):
+        self.people = people
+        self.selection_vals = {string.ascii_lowercase[x]: 0 for x in range(len(selections))}
+        self.selections = dict(zip(self.selection_vals.keys(), selections))
     
-    for option in options:
-        options_dict[option] += 4 - options.index(option)
+    def rank(self):
+        for _ in range(self.people):
+            for movie in self.selections.keys():
+                print(movie + ") " + self.selections[movie])
+            
+            options = input("Ranking (i.e. a,b,c): ").lower().replace(" ", "").split(",")
+            
+            for option in options:
+                self.selection_vals[option] += 3 - options.index(option)
 
-    print("\n" * 30)
+            print("\n" * 30)
     
-print({k: v for k, v in sorted(options_dict.items(), key=lambda item: item[1], reverse=True)})
+    def results(self):
+        print("Final Results: ")
+        sorted_vals = {k: v for k, v in sorted(self.selection_vals.items(), key=lambda item: item[1], reverse=True)}
+        for k, n in dict(zip(sorted_vals.keys(), range(1, len(self.selections) + 1))).items():
+            print(str(n) + ". " + self.selections[k])
+
+        print()
+        if input("See raw? (y/n): ").lower().strip() == "y":
+            print(self.selection_vals)
+
+if __name__ == "__main__":
+    people = int(input("People: "))
+
+    movie_input = input("Movies (Comma separated): ").title().replace(" ", "")
+    if not movie_input.isalnum:
+        raise Exception("Input error. Please try again.")
+    movie_input = movie_input.split(",")
+
+    poll = Poll(people, movie_input)
+    poll.rank()
+    poll.results()
