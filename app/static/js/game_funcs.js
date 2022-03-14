4cf91847a837693a720f98f005f2c63facc41225
AAWorks <axalonso12@gmail.com>
2022-03-13 21:50:41 -0400

    scaffold

---
+++ app/static/js/game_funcs.js
@@ -0,0 +1,38 @@
+/*
+-1: mine
+ f: flagged empty spot
+ F: flagged mine
+0-5: number of surrounding mines
+None: uncovered spot */
+
+IMG_CODES = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "F": "flag",
+    "M": "mine",
+    "?": "uncovered"
+}
+            
+var adjust_board = (arr) => {
+
+}
+
+var create_board = (width, height) => {
+    
+}
+    
+var bury_mines = (gameboard, n, x, y) => {
+
+}
+
+var get_mine_count = (gameboard, x, y) => {
+
+}
+
+var uncover_board = (gameboard, x, y) => {
+
+}
