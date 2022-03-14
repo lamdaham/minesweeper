b80f34311b35a4700f2be03be0e96fccb32fd9a4
AAWorks <axalonso12@gmail.com>
2022-03-13 21:51:11 -0400

    scaffold

---
+++ app/static/js/ui_funcs.js
@@ -0,0 +1,18 @@
+var gameboard = create_board(9, 9)
+var flagged_mines = 0
+var unflagged_mines = []
+var original = {}
+var gameover = False
+var first_uncover = True
+
+var flag = (e) => {
+
+}
+
+var uncover = (e) => {
+
+}
+
+var run = () => {
+    //not an actual function --> elements of the python run function should be factored into the other js methods
+}
