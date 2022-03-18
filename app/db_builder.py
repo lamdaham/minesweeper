17f8aef64d91da3da98903c0b8a3f8a811bc1f65
AAWorks <axalonso12@gmail.com>
2022-03-18 00:28:13 -0400

    big push to integrate dark mode and difficulties

--- app/db_builder.py
+++ app/db_builder.py
@@ -7,7 +7,7 @@ db = sqlite3.connect(DB_FILE, check_same_thread=False)
 def dbsetup():
   c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
   
-  command = "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+  command = "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, mode TEXT, difficulty TEXT)"
   c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
 
   db.commit() #save changes
@@ -29,7 +29,7 @@ def signup(username, password):
       return "Error: Username already exists"
 
   else:
-      c.execute('INSERT INTO user VALUES (null, ?, ?)', (username, password))
+      c.execute('INSERT INTO user VALUES (null, ?, ?, ?, ?)', (username, password, "light", "script kiddie"))
       
       db.commit()
       # Uses empty quotes since it will return false when checked as a boolean
@@ -48,3 +48,38 @@ def login(username, password):
 
   else:
       return True
+
+def get_mode(username):
+  c = db.cursor()
+
+  c.execute("""SELECT mode FROM user WHERE username=?""",[username])
+  result = c.fetchone()
+
+  return result
+
+def get_difficulty(username):
+  c = db.cursor()
+
+  c.execute("""SELECT difficulty FROM user WHERE username=?""",[username])
+  result = c.fetchone()
+
+  return result
+
+def change_mode(username):
+  c = db.cursor()
+
+  c.execute("""SELECT mode FROM user WHERE username=?""",[username])
+  result = c.fetchone()
+
+  if result == "dark":
+    c.execute('UPDATE user SET mode = ? WHERE username = ?', ("light", username))
+  else:
+    c.execute('UPDATE user SET mode = ? WHERE username = ?', ("dark", username))
+  db.commit()
+
+def change_difficulty(username, new_diff):
+  c = db.cursor()
+  
+  c.execute('UPDATE user SET difficulty = ? WHERE username = ?', (new_diff, username))
+
+  db.commit()
