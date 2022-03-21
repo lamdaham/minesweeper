c47355a67d702e39c3a2f57b7c80557a77d58011
AAWorks <axalonso12@gmail.com>
2022-03-21 09:47:44 -0400

    setup for config

--- app/__init__.py
+++ app/__init__.py
@@ -192,4 +192,4 @@ def other_result():
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
